e7691b9894ce538e935b87ba7a3429bddafeed60
savchenkorv96 <savchenkorv96@gmail.com>
2019-12-14 13:32:36 +0200

    registration repository in HubEnvironment

--- SoftwareCompany.Service/SoftwareCompany.Service.Core/Environment/HubEnvironment.cs
+++ SoftwareCompany.Service/SoftwareCompany.Service.Core/Environment/HubEnvironment.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SoftwareCompany.BLL.Activities.Contracts;
+using SoftwareCompany.BLL.Activities.Factory;
+using SoftwareCompany.BLL.Core.Contract;
+using SoftwareCompany.BLL.Core.Factory;
+using SoftwareCompany.BLL.Rules.Contract;
+using SoftwareCompany.BLL.Rules.Factory;
+using SoftwareCompany.DAL.Core;
+using SoftwareCompany.DAL.Core.Factory;
+using SoftwareCompany.DAL.Core.Factory.Contract;
 
 namespace SoftwareCompany.Service.Core.Environment
 {
     public class HubEnvironment
     {
-        public HubEnvironment()
+        public IUseCaseFactory UseCaseFactory { get; set; }
+
+        public HubEnvironment(ApplicationDbContext context)
         {
+            IValidationRuleFactory validationRuleFactory = new ValidationRuleFactory();
+            IRepositoryFactory repositoryFactory = new RepositoryFactory(context);
+            IActivitiesFactory activitiesFactory = new ActivitiesFactory(repositoryFactory, validationRuleFactory);
+
+            UseCaseFactory = new UseCaseFactory(activitiesFactory);
         }
     }
 }
