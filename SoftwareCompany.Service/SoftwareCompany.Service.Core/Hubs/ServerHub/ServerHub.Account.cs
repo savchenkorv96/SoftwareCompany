41efdae6e155a126265c71c35c38c8ca7972a01f
savchenkorv96 <savchenkorv96@gmail.com>
2019-12-14 13:39:23 +0200

    add method Login to ServerHub.Account

--- SoftwareCompany.Service/SoftwareCompany.Service.Core/Hubs/ServerHub/ServerHub.Account.cs
+++ SoftwareCompany.Service/SoftwareCompany.Service.Core/Hubs/ServerHub/ServerHub.Account.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using SoftwareCompany.BLL.Core.Contract;
+using SoftwareCompany.BLL.DomainEvents.AccountEvents.LoginEvents;
+using SoftwareCompany.DAL.Common.Entities;
+using SoftwareCompany.Service.Core.Helpers;
+using Task = System.Threading.Tasks.Task;
 
 namespace SoftwareCompany.Service.Core.Hubs.ServerHub
 {
     partial class ServerHub
     {
+        public async Task<OperationStatusInfo> Login(string login, string password)
+        {
+            return await Task.Run(() =>
+            {
+                OperationStatusInfo operationStatusInfo = new OperationStatusInfo(operationStatus: OperationStatus.Done);
+                LoginRequestEvent request = new LoginRequestEvent(login, password);
+
+                try
+                {
+                    LoginResponseEvent response =
+                        _hubEnvironment.UseCaseFactory.Create<IUseCase<LoginRequestEvent, LoginResponseEvent>>().Execute(request);
+
+                    operationStatusInfo.AttachedObject = response.Account;
+
+                    return operationStatusInfo;
+                }
+                catch (Exception ex)
+                {
+                    operationStatusInfo.OperationStatus = OperationStatus.Cancelled;
+                    operationStatusInfo.AttachedInfo = ex.Message;
+                }
+
+                return operationStatusInfo;
+            });
+        }
     }
 }
