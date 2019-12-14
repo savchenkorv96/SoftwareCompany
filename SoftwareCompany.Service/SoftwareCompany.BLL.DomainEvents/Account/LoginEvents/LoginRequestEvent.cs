8ab85b4e11b87be248a1a9ac49a14587cd8e1a1f
savchenkorv96 <savchenkorv96@gmail.com>
2019-12-14 12:16:50 +0200

    add bll events for auth

---
+++ SoftwareCompany.Service/SoftwareCompany.BLL.DomainEvents/Account/LoginEvents/LoginRequestEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareCompany.BLL.DomainEvents.Account.LoginEvents
+{
+    public sealed class LoginRequestEvent
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public LoginRequestEvent(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+    }
+}
---
+++ SoftwareCompany.Service/SoftwareCompany.BLL.DomainEvents/Account/LoginEvents/LoginResponseEvent.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SoftwareCompany.BLL.DomainEvents.Account.LoginEvents
+{
+    using DAL.Common.Entities;
+    public sealed class LoginResponseEvent
+    {
+        public Account Account { get; set; }
+
+        public LoginResponseEvent(Account account)
+        {
+            Account = account;
+        }
+    }
+}
