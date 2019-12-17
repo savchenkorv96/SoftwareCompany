627069dcd6d6bc56299b76b7bba8aafa7fbf8c55
savchenkorv96 <savchenkorv96@gmail.com>
2019-12-18 00:13:53 +0200

    add use case method to ServerHub.Customer

--- SoftwareCompany.Service/SoftwareCompany.Service.Core/Hubs/ServerHub/ServerHub.Customer.cs
+++ SoftwareCompany.Service/SoftwareCompany.Service.Core/Hubs/ServerHub/ServerHub.Customer.cs
@@ -1,10 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using SoftwareCompany.BLL.Core.Contract;
+using SoftwareCompany.BLL.DomainEvents.CustomerEvents.GetAllCustomerEvent;
+using SoftwareCompany.BLL.DomainEvents.CustomerEvents.GetCustomerByIdEvent;
+using SoftwareCompany.BLL.DomainEvents.TeamEvents.GetTeamByIdEvent;
+using SoftwareCompany.Service.Core.Helpers;
 
 namespace SoftwareCompany.Service.Core.Hubs.ServerHub
 {
     partial class ServerHub
     {
+        public async Task<OperationStatusInfo> GetAllCustomer()
+        {
+            return await Task.Run(() =>
+            {
+                OperationStatusInfo operationStatusInfo = new OperationStatusInfo(operationStatus: OperationStatus.Done);
+                GetAllCustomerRequestEvent request = new GetAllCustomerRequestEvent();
+
+                try
+                {
+                    GetAllCustomerResponseEvent response =
+                        _hubEnvironment.UseCaseFactory.Create<IUseCase<GetAllCustomerRequestEvent, GetAllCustomerResponseEvent>>().Execute(request);
+
+                    operationStatusInfo.AttachedObject = response.Customers;
+
+                    return operationStatusInfo;
+                }
+                catch (Exception ex)
+                {
+                    operationStatusInfo.OperationStatus = OperationStatus.Cancelled;
+                    operationStatusInfo.AttachedInfo = ex.Message;
+                }
+
+                return operationStatusInfo;
+            });
+        }
+
+        public async Task<OperationStatusInfo> GetCustomerId(int id)
+        {
+            return await Task.Run(() =>
+            {
+                OperationStatusInfo operationStatusInfo = new OperationStatusInfo(operationStatus: OperationStatus.Done);
+                GetCustomerByIdRequestEvent request = new GetCustomerByIdRequestEvent(id);
+
+                try
+                {
+                    GetCustomerByIdResponseEvent response =
+                        _hubEnvironment.UseCaseFactory.Create<IUseCase<GetCustomerByIdRequestEvent, GetCustomerByIdResponseEvent>>().Execute(request);
+
+                    operationStatusInfo.AttachedObject = response.Customer;
+
+                    return operationStatusInfo;
+                }
+                catch (Exception ex)
+                {
+                    operationStatusInfo.OperationStatus = OperationStatus.Cancelled;
+                    operationStatusInfo.AttachedInfo = ex.Message;
+                }
+
+                return operationStatusInfo;
+            });
+        }
     }
 }
