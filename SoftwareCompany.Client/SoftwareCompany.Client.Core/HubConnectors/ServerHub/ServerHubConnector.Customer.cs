637955c8cdd3f8355ef694189142a2563db9b9e4
savchenkorv96 <savchenkorv96@gmail.com>
2019-12-18 00:16:55 +0200

    add Customer method to ServerHubConnector.Customer

--- SoftwareCompany.Client/SoftwareCompany.Client.Core/HubConnectors/ServerHub/ServerHubConnector.Customer.cs
+++ SoftwareCompany.Client/SoftwareCompany.Client.Core/HubConnectors/ServerHub/ServerHubConnector.Customer.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using SoftwareCompany.Client.Common.Helpers;
+
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace SoftwareCompany.Client.Core.HubConnectors.ServerHub
 {
     partial class ServerHubConnector
     {
+        public async Task<OperationStatusInfo> GetAllCustomer()
+        {
+            return await _hubConnection.InvokeCoreAsync<OperationStatusInfo>("GetAllCustomer", new object[] { });
+        }
+
+        public async Task<OperationStatusInfo> GetCustomerById(int customerId)
+        {
+            return await _hubConnection.InvokeCoreAsync<OperationStatusInfo>("GetCustomerById", new object[] { customerId });
+        }
     }
 }
